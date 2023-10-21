33272e6eeb89af72eee7b6b6cd1b002146517a9b
Donte Littlejohn <donteravae@gmail.com>
2023-10-20 23:22:10 -0400

    added account navigation dropdown menu on hover

--- components/ApplicationHeader/AccountNavigation/AccountNavigation.tsx
+++ components/ApplicationHeader/AccountNavigation/AccountNavigation.tsx
@@ -1,24 +1,58 @@
+// REACT
+import { useState } from "react";
 // NEXT.JS
 import Image from "next/image";
 // INTERNAL
 import useAuth from "@/hooks/useAuth";
 // STYLES
 import styles from "./AccountNavigation.module.scss";
+import signOut from "@/actions/SignOut";
+import { useRouter } from "next/navigation";
 
 export default function AccountNavigation() {
-  const { currentProfile } = useAuth();
+  const router = useRouter();
+  const { currentProfile, logoutUser, selectProfile } = useAuth();
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const onSignOut = () => {
+    signOut();
+    logoutUser();
+    router.replace("/");
+  };
+
   return (
-    <nav className={styles.accountNav}>
-      <p>{currentProfile?.name}</p>
-      <Image
-        priority
-        width={50}
-        height={50}
-        className={styles.profilePic}
-        src={`https://api.multiavatar.com/${currentProfile?.id}.svg`}
-        alt="User Avatar"
-      />
-      <section className={styles.dropdown}></section>
-    </nav>
+    currentProfile && (
+      <nav
+        className={styles.accountNav}
+        onMouseEnter={() => setIsVisible(true)}
+        onMouseLeave={() => setIsVisible(false)}
+      >
+        <p>{currentProfile?.name}</p>
+        <Image
+          priority
+          width={50}
+          height={50}
+          className={styles.profilePic}
+          src={`https://api.multiavatar.com/${currentProfile?.id}.svg`}
+          alt="User Avatar"
+        />
+        <section
+          className={`${styles.dropdown} ${
+            isVisible ? styles.showDropdown : ""
+          }`}
+        >
+          <ul>
+            <li>My Account</li>
+            <li>Profiles</li>
+            <li>Help</li>
+            <li>
+              <button className={styles.signOutButton} onClick={onSignOut}>
+                Sign Out
+              </button>
+            </li>
+          </ul>
+        </section>
+      </nav>
+    )
   );
 }
