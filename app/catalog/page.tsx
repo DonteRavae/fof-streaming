abed8975e3ae3d1ef8c1ff55a631a7f2f78288fb
Donte Littlejohn <donteravae@gmail.com>
2023-11-06 11:51:03 -0500

    added SectionalVideoCardSliders to Catalog page for testing

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,9 +1,13 @@
+import SectionalVideoCardSlider from "@/components/SectionalVideoCardSlider/SectionalVideoCardSlider";
 import Showcase from "@/components/Showcase/Showcase";
 
 export default function CatalogOverviewPage() {
   return (
     <main>
       <Showcase />
+      <SectionalVideoCardSlider label="Continue Watching" />
+      <SectionalVideoCardSlider label="New on Force of Faith" />
+      <SectionalVideoCardSlider label="Trending Now" />
     </main>
   );
 }
