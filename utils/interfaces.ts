05e98232290e404418739f11bb3cf6f707ede78d
Donte Littlejohn <donteravae@gmail.com>
2023-11-27 15:15:03 -0500

    created Subscriber type for auth response and added subscription status field to IAuth interface.

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -1,11 +1,22 @@
 import { RowDataPacket } from "mysql2";
 
+// INTERFACES
+
 export interface IAuth extends RowDataPacket {
   id: string;
   ref: string;
   email: string;
   hash: string;
-  verified: boolean;
+  status:
+    | "active"
+    | "paused"
+    | "trialing"
+    | "incomplete"
+    | "past_due"
+    | "unpaid"
+    | "canceled"
+    | "incomplete_expired";
+  profiles: IProfile[];
   refresh_token: string;
   created_at: Date;
   last_modified: Date;
@@ -15,7 +26,14 @@ export interface IProfile extends RowDataPacket {
   id: string;
   name: string;
   auth_id: string;
-} 
+}
+
+// TYPES
+export type Subscriber = {
+  id: string;
+  email: string;
+  profiles: Profile[];
+};
 
 export type Profile = {
   id: string;
@@ -29,5 +47,5 @@ export type AuthResponse = {
 
 export type AuthResponsePayload = {
   message: string | null;
-  payload: Profile[] | null;
+  payload: Subscriber | null;
 };
