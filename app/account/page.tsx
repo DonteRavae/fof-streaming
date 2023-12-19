bb9d84138c1676739b1484c2afbadbb26c12dac1
Donte Littlejohn <donteravae@gmail.com>
2023-12-19 17:02:15 -0500

    created initial My Account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,3 +1,41 @@
-export default function AccountPage() {
-  return <main>Account Page</main>;
+// INTERNAL
+import FadeIn from "@/components/FadeIn/FadeIn";
+import getStripeDetails from "@/actions/GetStripeDetails.action";
+import cancelSubscription from "@/actions/CancelSubscription.action";
+import BillingDetails from "@/components/BillingDetails/BillingDetails";
+import AccountDetailsCard from "@/components/AccountDetailsCard/AccountDetailsCard";
+import AccountManagementDetails from "@/components/AccountManagementDetails/AccountManagementDetails";
+// STYLES
+import styles from "./page.module.scss";
+
+export default async function AccountPage() {
+  const stripeDetails = await getStripeDetails();
+  const cancelSubscriptionHandler = cancelSubscription;
+  return (
+    <main className={styles.accountPage}>
+      <FadeIn from="top">
+        <AccountDetailsCard label="Account Details">
+          <AccountManagementDetails />
+        </AccountDetailsCard>
+      </FadeIn>
+      <FadeIn from="bottom">
+        <AccountDetailsCard label="Billing">
+          {stripeDetails ? (
+            <BillingDetails
+              stripeDetails={stripeDetails!}
+              cancelSubscriptionHandler={cancelSubscriptionHandler}
+            />
+          ) : (
+            "There was an error loading billing details."
+          )}
+        </AccountDetailsCard>
+      </FadeIn>
+      {/* <FadeIn from="top">
+        <AccountDetailsCard label="Profiles" profilesManagement />
+      </FadeIn> */}
+      {/* <FadeIn from="top">
+        <AccountDetailsCard label="Security" security />
+      </FadeIn> */}
+    </main>
+  );
 }
