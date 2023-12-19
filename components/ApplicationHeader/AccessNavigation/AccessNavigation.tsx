2ed2981a32efbdee0cd379c768f77cda4dc0c4ed
Donte Littlejohn <donteravae@gmail.com>
2023-12-19 17:18:09 -0500

    added sign out link to AccessNavigation

--- components/ApplicationHeader/AccessNavigation/AccessNavigation.tsx
+++ components/ApplicationHeader/AccessNavigation/AccessNavigation.tsx
@@ -2,13 +2,40 @@
 
 // NEXT.JS
 import Link from "next/link";
+// INTERNAL
+import useAuth from "@/hooks/useAuth";
+import signOutUser from "@/actions/SignOut.action";
 // STYLES
 import styles from "./AccessNavigation.module.scss";
+import CallToActionButton from "@/components/CallToActionButton/CallToActionButton";
 
-export default function AccessNavigation() {
+export default function AccessNavigation({
+  signIn,
+  signOut,
+}: {
+  signIn?: boolean;
+  signOut?: boolean;
+}) {
+  const { logoutUser } = useAuth();
+
+  const onSignOut = async () => {
+    await signOutUser();
+    logoutUser();
+  };
+
   return (
     <nav className={styles.accessNav}>
-      <Link href="/access/signin">Sign In</Link>
+      {signIn ? (
+        <Link href="/access/signin">
+          <CallToActionButton>Sign In</CallToActionButton>
+          {/* <button>Sign In</button> */}
+        </Link>
+      ) : signOut ? (
+        <Link href="/">
+          <CallToActionButton onClick={onSignOut}>Sign Out</CallToActionButton>
+          {/* <button onClick={onSignOut}>Sign Out</button> */}
+        </Link>
+      ) : null}
     </nav>
   );
 }
