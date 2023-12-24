abcb10ea8fda234e9cb1fbf2552bd9a4db00863f
Donte Littlejohn <donteravae@gmail.com>
2023-12-24 10:41:05 -0500

    added initial upload page

---
+++ app/upload/page.tsx
@@ -0,0 +1,6 @@
+import ContentUploadForm from "@/components/ContentUploadForm/ContentUploadForm";
+import React from "react";
+
+export default function UploadPage() {
+  return <ContentUploadForm />;
+}
