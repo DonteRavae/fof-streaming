1afa1ff988df134492906d930942b76d600bd89d
Donte Littlejohn <donteravae@gmail.com>
2024-01-09 13:35:45 -0500

    converted showcase component from a slider to a single video display

--- components/Showcase/Showcase.tsx
+++ components/Showcase/Showcase.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+// NEXT.JS
+import Image from "next/image";
+
 // REACT
 import { useEffect, useRef, useState } from "react";
+
+// INTERNAL
+import videoPoster from "@/assets/video_poster.jpg";
 
 // STYLES
 import styles from "./Showcase.module.scss";
@@ -10,10 +16,11 @@ import { Icons } from "../Icons";
 export default function Showcase() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [mute, toggleMute] = useState<boolean>(true);
+  const [isPosterVisible, setIsPosterVisible] = useState<boolean>(true);
   useEffect(() => {
     setTimeout(() => {
       if (videoRef && videoRef.current) {
-        videoRef.current.play();
+        // videoRef.current.play();
       }
     }, 1000);
   }, [videoRef]);
@@ -27,8 +34,17 @@ export default function Showcase() {
 
   return (
     <section className={styles.showcase}>
-      <video ref={videoRef} muted>
-        <source type="video/mp4" src="/ShowcaseTestVideo.mp4" />
+      <Image
+        src={videoPoster}
+        alt=""
+        fill
+        className={`${styles.poster} ${!isPosterVisible ? styles.hide : ""}`}
+      />
+      <video ref={videoRef} muted poster="../../assets/video_poster.jpg">
+        <source
+          type="video/mp4"
+          src={`${process.env.AWS_PREVIEWS_URL!}/Preview_Test1.mp4`}
+        />
       </video>
       <button className={styles.volumeToggle} onClick={toggleVolume}>
         <Icons type={mute ? "volume-muted" : "volume-up"} />
